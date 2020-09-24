c0db47c6460362e56b0c42717ac16bbe1a1f3b2d
chegreyev <chegreyev@gmail.com>
2020-09-24 21:02:40 +0600

    description for burgers

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Burger
 
+
 class BurgerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Burger
-        fields = '__all__'
+        fields = ['title', 'price', 'image']
+
+    def to_representation(self, instance):
+        representation = super(BurgerSerializer, self).to_representation(instance)
+        representation['description'] = f'{representation["title"]}!\n\nЦена: {representation["price"]}₸'
+        return representation
